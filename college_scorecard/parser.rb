38eb1ec7b53dbfcdd782b77d6a78c0b4ef961d23
Pat Wey <patrickwey@gmail.com>
2016-02-03 13:21:30 -0700

    Finish comments!

--- college_scorecard/parser.rb
+++ college_scorecard/parser.rb
@@ -4,7 +4,8 @@ require 'csv'
 class Parser
   # another class method (`Parser.from_csv`)
   def self.from_csv(filepath)
+    # read the csv file
     CSV.read(filepath, headers: true, header_converters: :symbol)
-       .map { |r| r.to_h }
+       .map { |r| r.to_h } # create an array of the rows as hashes
   end
 end
