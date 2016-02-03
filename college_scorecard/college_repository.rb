097a1aac66e8db1c302dd7c224a5da4e247c6c45
Pat Wey <patrickwey@gmail.com>
2016-02-03 13:02:04 -0700

    More comments!

--- college_scorecard/college_repository.rb
+++ college_scorecard/college_repository.rb
@@ -1,8 +1,11 @@
+# give us access to the Parser class in parser.rb
 require_relative 'parser'
 
 class CollegeRepository
+  # a class method named data, called by `CollegeRepository.data`
   def self.data
-    filepath = File.expand_path("2013_college_scorecards.csv")
-    Parser.from_csv(filepath)
+    filepath = File.expand_path("2013_college_scorecards.csv") # basically: "give me the full path for this file"
+    
+    Parser.from_csv(filepath) # call the from_csv method on the Parser class, and pass in the filepath variable
   end
 end
