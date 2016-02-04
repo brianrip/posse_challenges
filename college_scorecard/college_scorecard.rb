f6b15db29a5ed66102e5ad8f48f6bfaf96f0b36e
Pat Wey <patrickwey@gmail.com>
2016-02-04 09:00:25 -0700

    Start work on AVGFACSAL

--- college_scorecard/college_scorecard.rb
+++ college_scorecard/college_scorecard.rb
@@ -1,45 +1,35 @@
-# give us access to the CollegeRepository class from college_repository.rb
 require_relative 'college_repository'
 
 class CollegeScoreboard
-  def initialize # <- when we call `CollegeScoreboard.new` (creates a new instance of this class)
-
-    # 'memoize' the csv data - basically: "create this variable, but since it takes a while, check to see if we've already created it, and if so, use that one."
+  def initialize
     @data ||= CollegeRepository.data
   end
 
   def start(fn_name, arg)
-    #       ^function name, argument for that function
-    send(fn_name.to_sym, arg) # probably don't use this....   :-O  !!!!
-    #     ^call the function name passed in, and pass it the argument
+    send(fn_name.to_sym, arg)
   end
 
   def by_state(state)
-    # access the @data instance variable (this is the parsed csv data)
-    @data.reduce([]) do |colleges_in_state, college_data| # reduce the data to an array (named colleges_in_state locally)
-
-      if college_data[:stabbr] == state # if the "stabbr" value is equal to the state string passed into this method...
-
-        # ...then add the college name (college_data[:instnm] in our data) to the colleges_in_state array.
+    @data.reduce([]) do |colleges_in_state, college_data|
+      if college_data[:stabbr] == state
         colleges_in_state.push(college_data[:instnm])
       end
-
-      colleges_in_state # return this array at the end of the loop each time.
+      colleges_in_state
     end
   end
 
-
+  def top_average_faculty_salary(num)
+    @data.select { |college_data| college_data[:avgfacsal] != "NULL" }
+    .sort_by { |college_data| college_data[:avgfacsal] }
+    require 'pry'; binding.pry
+    .map { |college_data| college_data[:instnm] }
+    .first(num.to_i)
+  end
 end
 
-# when this file is run:
-# 1) create a new instance of CollegeScoreboard
-# 2) call the `start` method on that instance
-# 3) pass it the arguments inputted in the command line
+
+
+
+
 
 puts CollegeScoreboard.new.start(ARGV[0], ARGV[1])
-
-# $ <- means a command in the command line
-# e.g. `$ ruby college_scorecard.rb by_state AK`
-
-# ARGV[0] => "by_state"
-# ARGV[1] => "AK"
