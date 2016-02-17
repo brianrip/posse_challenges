af87f3e9f067b18d3463c92472c93079fd9fd1a4
Taylor Moore <Tmoore2272@gmail.com>
2016-02-17 09:08:47 -0700

    Wednesday work

--- people_db/people_database.rb
+++ people_db/people_database.rb
@@ -7,13 +7,41 @@ class PeopleDatabase
 
   def add(data)
     @database << Person.new(data)
-    require 'pry' ; binding.pry
+  end
+
+  def remove(email)
+    @database.delete_if do |data|
+      data.email == email
+    end
+  end
+
+  def find_by(state)
+    @database.select do |data|
+      data.state == state
+    end
+  end
+
+  def where(email)
+    @database.map do |data|
+      data.email
+    end.join(', ')
+  end
+
+  def count(state)
+    @database.count do |data|
+      data.state == state
+    end
   end
 end
+
+
 
 
 if __FILE__ == $0
   people = []
   people_db = PeopleDatabase.new(people)
   people_db.add(first_name: "Tess", last_name: "Griffin", email: "tess@turing.io", state: "CO")
+  people_db.add(first_name: "Tess", last_name: "Griffin", email: "BOSSS!", state: "CO")
+  people_db.remove("tess@turing.io")
+  # require 'pry' ; binding.pry
 end
